8ebc8d1c304c6ab49818e8eef471f0a3208efc02
neurona <fwneurona@gmail.com>
2021-01-20 01:46:54 -0600

    sa

--- app/controllers/academicos_controller.rb
+++ app/controllers/academicos_controller.rb
@@ -19,12 +19,28 @@ class AcademicosController < ApplicationController
     if @tipo == 2
        @actividad = Actividad.where(periodo:2020).where("estado in ('S')").order(:producto_id).order(:id).joins(:autores).where(autores: {persona_id:personaid}) 
     end  
+    if @tipo == 3
+      @actividad = Actividad.where(periodo:2020).where("estado in ('A','U','G','Z','S','C')").order(:producto_id).order(:id).joins(:autores).where(autores: {persona_id:personaid}) 
+    end  
      #Autor.where(:persona_id=>personaid).each do |a|
      #    idsActiv.push(a.actividad_id) 
      # end
        #@actividad = Actividad.where(:id=>idsActiv).where(periodo:2020).where("estado in ('C','S','Z','W')").order(:producto_id).order(:id)
      #  @actividad = Actividad.where(:id=>idsActiv).where(periodo:2020).where("estado in ('A','U','C','S','Z','W')").order(:producto_id).order(:id)
        @persona = Persona.find(personaid)
+  end
+
+  def detalleactividad
+      idactividad = params[:id]
+      idacademico = params[:idacademico]
+      @academico = Academico.find(idacademico)
+      @actividad = Actividad.find(idactividad)
+      @producto = Producto.find(@actividad.producto_id)
+      @dataAct = Actividad.find(@actividad.id)
+      @captura = Persona.find(@actividad.personaid)
+      @tipo_cap = @captura.tipopersona_id
+      personaid = @academico.persona_id
+      @persona = Persona.find(personaid)
   end
 
   def activar
