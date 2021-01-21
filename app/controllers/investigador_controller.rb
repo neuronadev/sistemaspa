a7294f094811a28e99ec3d6575d7be628136076d
neurona <fwneurona@gmail.com>
2021-01-21 12:36:55 -0600

    observa

--- app/controllers/investigador_controller.rb
+++ app/controllers/investigador_controller.rb
@@ -17,7 +17,7 @@ class InvestigadorController < ApplicationController
          idsActiv.push(a.actividad_id) 
       end
        #@actividad = Actividad.where(:id=>idsActiv).where(periodo:2020).where("estado in ('C','S','Z','W')").order(:producto_id).order(:id)
-       @actividad = Actividad.where(:id=>idsActiv).where(periodo:2020).where("estado in ('A','U','C','S','Z','W')").order(:id)
+       @actividad = Actividad.where(:id=>idsActiv).where(periodo:2020).where("estado in ('A','U','C','S','Z','W','G')").order(:id)
        @persona = Persona.find(personaid.to_i)
   end
 
