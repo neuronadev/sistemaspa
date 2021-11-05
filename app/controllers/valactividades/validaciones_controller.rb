3d73f38c1251e5fffaf5cb3a530e3a311a64bc6e
neurona <fwneurona@gmail.com>
2021-11-05 13:22:50 -0600

    updated

--- app/controllers/valactividades/validaciones_controller.rb
+++ app/controllers/valactividades/validaciones_controller.rb
@@ -1,6 +1,35 @@
 class Valactividades::ValidacionesController < ApplicationController
   layout 'validaactividades'
   def index
-    @actividades = Actividad.where(estado:'C').order(:id)
+    
+      tipo = params[:tipo]  
+      anio = 2021
+      @actividades = ''
+      invest = [] 
+      tecs = []
+
+      Usuario.where(rol:'I').each do |u|
+        if u.persona.estado == 'A'
+            invest << u.persona_id
+        end  
+      end
+      Usuario.where(rol:'T').each do |u|
+        if u.persona.estado == 'A'
+            tecs << u.persona_id
+        end  
+      end
+
+      if tipo == "A" 
+          @actividades = Actividad.where(estado:'C', periodo:anio).order(:id)
+      end 
+      
+      if tipo == "I" 
+          @actividades = Actividad.where(estado:'C', periodo:anio, personaid:invest).order(:id)
+      end 
+      
+      if tipo == "T" 
+          @actividades = Actividad.where(estado:'C', periodo:anio, personaid:tecs).order(:id)
+      end 
+
   end
 end
