7f3137aab315491cfdb9d3f82c08dd254cba2162
neurona <fwneurona@gmail.com>
2022-03-17 13:01:07 -0600

    updated

--- app/javascript/controllers/aceptarprod_controller.js
+++ app/javascript/controllers/aceptarprod_controller.js
@@ -4,7 +4,7 @@ export default class extends Controller{
 
     vbproductos(event){
         event.preventDefault()
-        if (confirm('Con esta acción esta aceptando la evaluación que se realizó sobre los productos académicos 2021 y serán las base para el cálculo de estímulos. ¿Desea continuar?')) {
+        if (confirm('Con esta acción esta aceptando la evaluación que se realizó sobre los productos académicos 2021 y serán la base para el cálculo de estímulos. ¿Desea continuar?')) {
              var el = document.getElementById("vbprod")
              let id = event.params.id
              
