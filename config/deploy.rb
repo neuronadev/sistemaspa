e5517045b28c8fc62c654626a2024e1090ca7916
neuronadev <fwneurona@gmail.com>
2022-06-09 16:32:16 -0500

    up_interfaz

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.2"
+lock "~> 3.17.0"
 
 set :application, "sistemaspa"
 set :repo_url, "https://github.com/neuronadev/sistemaspa.git"
