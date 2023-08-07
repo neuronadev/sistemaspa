ec01673f039fa55b41c2ff4eb059c2a9692746bf
neuronadev <fwneurona@gmail.com>
2023-08-07 12:27:16 -0600

    up

--- app/controllers/auditoria_controller.rb
+++ app/controllers/auditoria_controller.rb
@@ -9,37 +9,57 @@ class AuditoriaController < ApplicationController
     tecnicos = Array.new
     @listado = Array.new
     Persona.where(:tipopersona_id=>4).order(:paterno).each do |p|
-      tecnicos.push(p.academico.id)
+      tecnicos.push(p.id)
     end
     
     tecnicos.each do |t|
-       evaltec = 0.0
-       Sustantiva.where("academico_id=? and estado = 'C' and anio=2021",t).each do |s|
-          if s.calidad.present? && s.eficiencia.present?
-              evaltec = evaltec + (  (s.porcentaje/100)*(((s.calidad/10)+(s.eficiencia/10))/2)  )
-          end      
-       end
-       @listado.push([t,evaltec.round(2)])     
+        cal_final = 0.0
+        Evaltecnico.where(persona_id: t, anio:2022).each do |s|
+            s.itemsustantivas.each do |item| 
+                  porcentaje = item.porcentaje
+                  calidad = 0.0
+                  eficiencia = 0.0
+                  cal_item = 0.0
+                  if item.calificaciones.any?
+                        calidad = !item.calificaciones.where(tipo:'CA').first.nil? ?  item.calificaciones.where(tipo:'CA').first.calificacion : 0.0
+                        eficiencia = !item.calificaciones.where(tipo:'EF').first.nil? ?  item.calificaciones.where(tipo:'EF').first.calificacion : 0.0
+                  end  
+                  cal_item = (porcentaje.to_f/100) * ((calidad/10) + (eficiencia/10))/2
+                  cal_final += cal_item
+            end
+        end
+        @listado.push([t, cal_final.round(2)])
     end 
+
   end
 
   def sustantivas
     idacad = params[:id] 
     a = Academico.find(idacad)
     p = Persona.find(a.persona_id)
-    @t = (p.paterno||'') + " " + (p.materno||'') + (p.nombre||'')
-    @sust = Sustantiva.where("academico_id=? and estado != 'X' and anio=2021",idacad)
     
-    @tot_act = Sustantiva.where("academico_id=? and estado != 'X' and anio=2021",idacad).count()
+    @t = (p.paterno||'') + " " + (p.materno||'') + (p.nombre||'')
+
+    @sust = Evaltecnico.where(persona_id: p.id, anio:2022).first
+    @tot_act = @sust.itemsustantivas.count
+
     @prom = 0.0
     evaltec = 0.0
-    Sustantiva.where("academico_id=? and estado = 'C' and anio=2021",idacad).each do |s|
-      if s.calidad.present? && s.eficiencia.present?
-          evaltec = evaltec + (  (s.porcentaje/100)*(((s.calidad/10)+(s.eficiencia/10))/2)  )
-      end 
-    end
+
+        @sust.itemsustantivas.each do |item| 
+            porcentaje = item.porcentaje
+            calidad = 0.0
+            eficiencia = 0.0
+            cal_item = 0.0
+            if item.calificaciones.any?
+                 calidad = !item.calificaciones.where(tipo:'CA').first.nil? ?  item.calificaciones.where(tipo:'CA').first.calificacion : 0.0
+                 eficiencia = !item.calificaciones.where(tipo:'EF').first.nil? ?  item.calificaciones.where(tipo:'EF').first.calificacion : 0.0
+            end  
+            cal_item = (porcentaje.to_f/100) * ((calidad/10) + (eficiencia/10))/2
+            evaltec += cal_item
+        end
     
-    @prom = evaltec.round(2)
+      @prom = evaltec.round(2)
   end
   
   def adicionales
@@ -47,18 +67,28 @@ class AuditoriaController < ApplicationController
     tecnicos = Array.new
     @listado = Array.new
     Persona.where(:tipopersona_id=>4).order(:paterno).each do |p|
-      tecnicos.push(p.academico.id)
+      tecnicos.push(p.id)
     end
     
     tecnicos.each do |t|
-       evaltec = 0.0
-       Sustantiva.where("academico_id=? and estado = 'C' and anio=2021",t).each do |s|
-          if s.calidad.present? && s.eficiencia.present?
-              evaltec = evaltec + (  (s.porcentaje/100)*(((s.calidad/10)+(s.eficiencia/10))/2)  )
-          end      
+       cal_final = 0.0
+       Evaltecnico.where(persona_id: t, anio:2022).each do |s|
+            s.itemsustantivas.each do |item| 
+                  porcentaje = item.porcentaje
+                  calidad = 0.0
+                  eficiencia = 0.0
+                  cal_item = 0.0
+                  if item.calificaciones.any?
+                        calidad = !item.calificaciones.where(tipo:'CA').first.nil? ?  item.calificaciones.where(tipo:'CA').first.calificacion : 0.0
+                        eficiencia = !item.calificaciones.where(tipo:'EF').first.nil? ?  item.calificaciones.where(tipo:'EF').first.calificacion : 0.0
+                  end  
+                  cal_item = (porcentaje.to_f/100) * ((calidad/10) + (eficiencia/10))/2
+                  cal_final += cal_item
+            end
        end
-       @listado.push([t,evaltec.round(2)])     
+       @listado.push([t, cal_final.round(2)])
     end 
+
   end
 
   def detalle
@@ -66,26 +96,26 @@ class AuditoriaController < ApplicationController
    
     a = Academico.find(idacad)
     p = Persona.find(a.persona_id) 
-  
-    #academicos = Academico.all
-    tecnicos = Array.new
+    evaltec = 0.0
     @listado = Array.new
 
-    #Persona.where(:id=>p.id).order(:paterno).each do |p|
-      tecnicos.push(a.id)
-    #end
-    
-    tecnicos.each do |t|
-       evaltec = 0.0
-       Sustantiva.where("academico_id=? and estado = 'C' and anio=2021",t).each do |s|
-          if s.calidad.present? && s.eficiencia.present?
-              evaltec = evaltec + (  (s.porcentaje/100)*(((s.calidad/10)+(s.eficiencia/10))/2)  )
-          end      
+       cal_final = 0.0
+       Evaltecnico.where(persona_id: p.id, anio:2022).each do |s|
+            s.itemsustantivas.each do |item| 
+                  porcentaje = item.porcentaje
+                  calidad = 0.0
+                  eficiencia = 0.0
+                  cal_item = 0.0
+                  if item.calificaciones.any?
+                        calidad = !item.calificaciones.where(tipo:'CA').first.nil? ?  item.calificaciones.where(tipo:'CA').first.calificacion : 0.0
+                        eficiencia = !item.calificaciones.where(tipo:'EF').first.nil? ?  item.calificaciones.where(tipo:'EF').first.calificacion : 0.0
+                  end  
+                  cal_item = (porcentaje.to_f/100) * ((calidad/10) + (eficiencia/10))/2
+                  cal_final += cal_item
+            end
        end
-       if evaltec >= 8.5 
-         @listado.push([t,evaltec.round(2)])     
-       end  
-    end
+
+    @listado.push([idacad, cal_final.round(2)])          
   end
 
 
@@ -101,11 +131,11 @@ class AuditoriaController < ApplicationController
       Autor.where(:persona_id=>personaid).each do |a|
          idsActiv.push(a.actividad_id) 
       end
-       @actividad = Actividad.where(:id=>idsActiv).where("estado in ('S','W') and periodo=2021").order(:producto_id).order(:id)
+       @actividad = Actividad.where(:id=>idsActiv).where("estado in ('S','W') and periodo=2022").order(:producto_id).order(:id)
   end
   
   def cancelados
-    @folios = Actividad.where(periodo:2021, estado:['A','C','U','G','D','Z','W','X']).order(:id)
+    @folios = Actividad.where(periodo:2022, estado:['A','C','U','G','D','Z','W','X']).order(:id)
                                                     
   end
 
